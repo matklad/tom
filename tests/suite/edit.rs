29a8ffe4976561c7ec9c8fc6fcfcd962abfaed77
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-06-12 13:19:08 +0300

    Test for swap.
    Stack overflows atm

--- tests/suite/edit.rs
+++ tests/suite/edit.rs
@@ -1,1 +1,22 @@
-//TODO
+use tom::{
+    Edit,
+    ast::KeyValueOwner,
+};
+use {check_edit};
+
+#[test]
+fn test_swap() {
+    check_edit(
+        "foo = true\nbar = false",
+        "bar = false\nfoo = true",
+        |doc| {
+            let mut edit = Edit::new(doc);
+            let ast = doc.ast();
+            let foo = ast.entries().nth(0).unwrap();
+            let bar = ast.entries().nth(1).unwrap();
+            edit.replace(foo, bar);
+            edit.replace(bar, foo);
+            edit.finish()
+        }
+    );
+}
