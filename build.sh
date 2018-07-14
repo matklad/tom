08a4b817cc76a2d75a14c2b7d81962b2043b6d89
Aleksey Kladov <aleksey.kladov@gmail.com>
2018-07-15 01:33:45 +0300

    Remove wasm-opt, it breaks things

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
-set -e
+set -ex
 
-cargo build \
+cargo +nightly build \
       --manifest-path ./tom_wasm/Cargo.toml \
       --target wasm32-unknown-unknown \
       --release
 
 TARGET_DIR=tom_wasm/target/wasm32-unknown-unknown/release
 
-wasm-opt -Os --dce \
-         $TARGET_DIR/tom_wasm.wasm \
-         -o $TARGET_DIR/main.wasm
+cp $TARGET_DIR/tom_wasm.wasm $TARGET_DIR/main.wasm
 
 wasm-bindgen --no-modules --no-typescript \
              $TARGET_DIR/main.wasm \
--- main.js
+++ main.js
@@ -3,6 +3,66 @@
                     var wasm;
                     const __exports = {};
                     
+
+let cachedEncoder = new TextEncoder('utf-8');
+
+let cachegetUint8Memory = null;
+function getUint8Memory() {
+    if (cachegetUint8Memory === null ||
+        cachegetUint8Memory.buffer !== wasm.memory.buffer)
+        cachegetUint8Memory = new Uint8Array(wasm.memory.buffer);
+    return cachegetUint8Memory;
+}
+
+function passStringToWasm(arg) {
+
+    const buf = cachedEncoder.encode(arg);
+    const ptr = wasm.__wbindgen_malloc(buf.length);
+    getUint8Memory().set(buf, ptr);
+    return [ptr, buf.length];
+}
+
+let cachedDecoder = new TextDecoder('utf-8');
+
+function getStringFromWasm(ptr, len) {
+    return cachedDecoder.decode(getUint8Memory().subarray(ptr, ptr + len));
+}
+
+let cachedGlobalArgumentPtr = null;
+function globalArgumentPtr() {
+    if (cachedGlobalArgumentPtr === null)
+        cachedGlobalArgumentPtr = wasm.__wbindgen_global_argument_ptr();
+    return cachedGlobalArgumentPtr;
+}
+
+let cachegetUint32Memory = null;
+function getUint32Memory() {
+    if (cachegetUint32Memory === null ||
+        cachegetUint32Memory.buffer !== wasm.memory.buffer)
+        cachegetUint32Memory = new Uint32Array(wasm.memory.buffer);
+    return cachegetUint32Memory;
+}
+
+__exports.parse = function(arg0) {
+    const [ptr0, len0] = passStringToWasm(arg0);
+    const retptr = globalArgumentPtr();
+    try {
+        wasm.parse(retptr, ptr0, len0);
+        const mem = getUint32Memory();
+        const ptr = mem[retptr / 4];
+        const len = mem[retptr / 4 + 1];
+        const realRet = getStringFromWasm(ptr, len).slice();
+        wasm.__wbindgen_free(ptr, len * 1);
+        return realRet;
+    } finally {
+        wasm.__wbindgen_free(ptr0, len0 * 1);
+    }
+};
+
+__exports.__wbindgen_throw = function(ptr, len) {
+    throw new Error(getStringFromWasm(ptr, len));
+};
+
                     function init(wasm_path) {
                         return fetch(wasm_path)
                             .then(response => response.arrayBuffer())
